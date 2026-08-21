5bdf58481b9390ac58c0e2c118f7f723842b9c98
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-21 12:57:29 +0900

    コードビハインドを実装

--- ViewCs/Views/MainWindow.xaml.cs
+++ ViewCs/Views/MainWindow.xaml.cs
@@ -12,14 +12,39 @@ namespace  ViewCs.Views  {
 public  partial class  MainWindow : Window
 {
 
-    //----------------------------------------------------------------
-    /**   デフォルトコンストラクタ。
-    **
-    **/
-    public  MainWindow()
-    {
-        InitializeComponent();
-    }
+//----------------------------------------------------------------
+/**   デフォルトコンストラクタ。
+**
+**/
+public  MainWindow()
+{
+    InitializeComponent();
+    this.Loaded += MainWindow_Loaded;
+}
+
+private  void
+MainWindow_Loaded(object sender, RoutedEventArgs e)
+{
+    UpdateScrollRange();
+}
+
+private  void
+UpdateScrollRange()
+{
+}
+
+
+private  void
+VerticalScroll_Scroll(object sender, ScrollEventArgs e)
+{
+}
+
+
+protected  override  void
+OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+{
+    base.OnRenderSizeChanged(sizeInfo);
+}
 
 
 }   //  End class  MainWindow
