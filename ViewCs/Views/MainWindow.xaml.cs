fa3466672851efcdde60f73b5663331dd3a5cd3a
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-21 21:01:05 +0900

    コードビハインドを実装

--- ViewCs/Views/MainWindow.xaml.cs
+++ ViewCs/Views/MainWindow.xaml.cs
@@ -11,6 +11,9 @@ namespace  WpfHexEditor.Views  {
 public  partial class  MainWindow : Window
 {
 
+private   byte[]    m_dummyData;
+private   const int BytesPerRow = 16;
+
 //----------------------------------------------------------------
 /**   デフォルトコンストラクタ。
 **
@@ -18,6 +21,11 @@ public  partial class  MainWindow : Window
 public  MainWindow()
 {
     InitializeComponent();
+
+    m_dummyData = new byte[1048575];
+    new Random().NextBytes(this.m_dummyData);
+
+    HexEditor.setData(this.m_dummyData);
     this.Loaded += MainWindow_Loaded;
 }
 
@@ -30,12 +38,19 @@ MainWindow_Loaded(object sender, RoutedEventArgs e)
 private  void
 UpdateScrollRange()
 {
+    int  totalRows = (int)Math.Ceiling((double)(m_dummyData.Length / BytesPerRow);
+    int  visibleRows = (int)(HexEditor.AcutualHeight / HexEditor.RowHeight);
+
+    VerticalScroll.Minimum = 0;
+    VerticalScroll.Maximum = Math.Max(0, totalRows - visibleRows);
+    VerticalScroll.ViewportSize = visibleRows;
 }
 
 
 private  void
 VerticalScroll_Scroll(object sender, ScrollEventArgs e)
 {
+    HexEditor.CurrentRowOffset = (int)e.NewValue;
 }
 
 
@@ -43,6 +58,9 @@ protected  override  void
 OnRenderSizeChanged(SizeChangedInfo sizeInfo)
 {
     base.OnRenderSizeChanged(sizeInfo);
+    if ( this.m_dummyData != null ) {
+        UpdateScrollRange();
+    }
 }
 
 
